16852d75be241df7ca48adcbbf4490391580fffa
EstebanGeo <gabriele.barriosm66@gmail.com>
2024-08-31 16:07:48 -0500

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,16 @@
+meme_dict = {
+"GG": "abreviacion de goog game o buen juego o que el juego lo hizo bien"
+"SHHIPPEO" : " el acto de apoyar o desear una relación romántica entre dos personas"
+"AESTHETIC" : "Estético, perfecto, que gusta"
+"BUGUEARSE" : "quedarse atontao"
+"CRINGE": "Vergüenza ajena, desagrado, que genera rechazo: 
+    "me da cringe su forma de hablar"
+"LIT" : "literal"
+"EVENTO CANONICO": "Suceso por el que todo el mundo pasa al menos una vez en la vida."}
+
+word = input("escribe una palabra que no entiendas (¡con mayusculas !):  ")
+
+if meme in meme_dict.keys():
+    #¿que debemos haver si se encuentra la palabra?
+    else:
+        #¿Que hacer si no se encuentra la palabra?
